6614375929d8c03a550fd87800adcca7d228bb9d
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-07-08 13:41:24 +0300

    Update

--- nextjs-shop/src/app/page.tsx
+++ nextjs-shop/src/app/page.tsx
@@ -4,6 +4,7 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check } from "lucide-react";
 import { Star } from "lucide-react";
 import Phone from "@/components/Phone";
+import { Icons } from "@/components/icons";
 
 export default function Home() {
   return (
@@ -94,16 +95,43 @@ export default function Home() {
             </div>
           </div>
 
-          <div className='col-span-full lg:col-span-1 w-full flex justify-center 
-          px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
-            <div className = 'relative md:max-w-xl'>
-              <img src='/your-image.png'alt='' className='absolute-w-40 lg:w-52 left-56 -top-20 select-none
-              hidden sm:block lg:hidden xl:black'/>
-              <img src='/line.png'alt='' className= 'absolute w-20 -left-6 -bottom-6 select-none'/>
-              <Phone className='w-64' imgSrc="'testimonials/1.jpg"/>
+          <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
+            <div className='relative md:max-w-xl'>
+              <img
+                src='/your-image.png'
+                alt=''
+                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+              />
+              <img
+                src='/line.png'
+                alt=''
+                className='absolute w-20 -left-6 -bottom-6 select-none'
+              />
+              <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+      {/* value proposition section */}
+      <section className='bg-slate-100 py-24'>
+        <MaxWidthWrapper className='flex flex-col items-center gap-16
+        sm:gap-32'>
+          <div className='flex flex-col lg:flex-row items-center gap-4
+          sm:gap-6'>
+            <h2 className="order-1 mt-3 tracking-tight text-center text-balance
+            !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our {' '}
+              <span className='relative px-2'>
+                customers
+                <Icons.underline className='hidden sm:block
+                pointer-events-none absolute inset-x-o -bottom-6
+                text-green-500'/>
+                </span> {' '}
+                say
+                </h2>
+            <img src = '/snake-2.png' className='w-24 order-0 lg:order-2' alt=''/>
+            </div>
+          </MaxWidthWrapper>
       </section>
     </div>
   );
